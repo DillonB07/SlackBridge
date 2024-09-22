7face6942fe9aad74b4c003f22b0a55746f4e2b9
Dillon Barnes <dillonb07dev@gmail.com>
2024-09-22 15:06:54 +0100

    initial commit

---
+++ src/client/java/studio/dillonb07/slackbridge/client/SlackbridgeClient.java
@@ -0,0 +1,10 @@
+package studio.dillonb07.slackbridge.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SlackbridgeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/studio/dillonb07/slackbridge/Slackbridge.java
@@ -0,0 +1,10 @@
+package studio.dillonb07.slackbridge;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Slackbridge implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
